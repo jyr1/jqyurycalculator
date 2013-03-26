bef14e99d18daf9d5b3e749b1fefbb2faca11524
jyr1 <jyri.syvaoja@gmail.com>
2013-03-26 15:14:08 +0200

    C and . buttons are working

--- calc.js
+++ calc.js
@@ -3,6 +3,19 @@
  */
 $(document).ready(function() {
  $('button').click(function() {
-        $('.display').text($(this).data('value'));
+        //alert($('.display').data('hascomma'));
+        if ( $(this).data('value') == 'C') {
+                $('.display').text('');
+                $('.display').data('hascomma', false);
+        } else {
+                if ( $(this).data('value') == '.' && $('.display').data('hascomma')==false ){
+                        $('.display').data('hascomma', true);
+                        var displayContent = $('.display').text() + $(this).data('value');
+                        $('.display').text(displayContent);
+                } else if ($(this).data('value')!='.'){
+                        var displayContent = $('.display').text() + $(this).data('value');
+                        $('.display').text(displayContent);
+                }
+        }
  });
 });
