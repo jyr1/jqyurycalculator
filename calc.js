4278ff992330c3118b6335d0729a76244e8b72cb
Jyri Syväoja <jyri.syvaoja@gmail.com>
2013-03-26 18:24:32 +0200

    Added operator buttons

--- calc.js
+++ calc.js
@@ -2,11 +2,12 @@
  * @author syvaoja
  */
 $(document).ready(function() {
- $('button').click(function() {
-        //alert($('.display').data('hascomma'));
+ $('.nappi').click(function() {
+        //C clear 'screen'
         if ( $(this).data('value') == 'C') {
                 $('.display').text('');
                 $('.display').data('hascomma', false);
+		// number buttons and .
         } else {
                 if ( $(this).data('value') == '.' && $('.display').data('hascomma')==false ){
                         $('.display').data('hascomma', true);
@@ -17,5 +18,18 @@ $(document).ready(function() {
                         $('.display').text(displayContent);
                 }
         }
+ });
+ $('.oper').click(function() {
+ 		// operators +,-,* and / and actual calculations
+ 		if( $(operators).data('hasoperand')==false ) {
+			$(operators).data('hasoperand', true);
+			$('.operatorplace').text( $(this).data('value'));
+			// convert display to the number
+			var result = +$('.display').text();
+			// clear 'screen'
+			$('.display').text('');
+			// do math
+			
+		}
  });
 });
